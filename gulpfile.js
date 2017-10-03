15bf0305acd83faf8bdffaa975f7c452196e946d
John Melville <johnmelvillewebdesign@outlook.com>
2017-10-03 17:26:43 +0100

    merge stream added

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
+var browserify = require('gulp-browserify');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var merge = require('merge-stream');
 
 var SOURCEPATHS = {
   sassSource : 'src/scss/*.scss',
@@ -17,14 +21,37 @@ var APPPATH = {
   fonts: 'app/fonts'
 }
 
+gulp.task('clean-html', function(){
+  return gulp.src(APPPATH.root + '/*.html', {read: false, force: true})
+    .pipe(clean());
+});
+
+gulp.task('clean-scripts', function(){
+  return gulp.src(APPPATH.js + '/*.js', {read: false, force: true})
+    .pipe(clean());
+});
+
 gulp.task('sass', function(){
-  return gulp.src(SOURCEPATHS.sassSource)
-      .pipe(autoprefixer())
-      .pipe(sass({outputStyle: 'expanded'}).on('error',sass.logError))
+  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+  var sassFiles;
+
+
+  sassFiles =  gulp.src(SOURCEPATHS.sassSource)
+    .pipe(autoprefixer())
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    return merge(bootstrapCSS, sassFiles )
+      .pipe(concat('app.css'))
       .pipe(gulp.dest(APPPATH.css));
 });
 
-gulp.task('copy', function(){
+gulp.task('scripts', ['clean-scripts'], function(){
+  gulp.src(SOURCEPATHS.jsSource)
+    .pipe(concat('main.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest(APPPATH.js))
+});
+
+gulp.task('copy',['clean-html'], function(){
   gulp.src(SOURCEPATHS.htmlSource)
     .pipe(gulp.dest(APPPATH.root))
 });
@@ -37,9 +64,10 @@ gulp.task('serve', ['sass'], function(){
   })
 });
 
-gulp.task('watch', ['serve', 'sass','copy'], function(){
+gulp.task('watch', ['serve', 'sass','copy','clean-html','clean-scripts','scripts'], function(){
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
   gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+  gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
 });
 
 gulp.task('default',['watch']);
